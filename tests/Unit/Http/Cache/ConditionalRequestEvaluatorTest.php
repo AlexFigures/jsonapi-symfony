13fcc7dd3df46acd808a3b7e169af2d9f2bd7655
Aleksandr <alexfigures@mail.ru>
2025-10-06 09:56:18 +0300

    Expand conditional request evaluator coverage

--- tests/Unit/Http/Cache/ConditionalRequestEvaluatorTest.php
+++ tests/Unit/Http/Cache/ConditionalRequestEvaluatorTest.php
@@ -8,6 +8,7 @@ use JsonApi\Symfony\Http\Cache\ConditionalRequestEvaluator;
 use JsonApi\Symfony\Http\Error\ErrorBuilder;
 use JsonApi\Symfony\Http\Error\ErrorMapper;
 use JsonApi\Symfony\Http\Exception\PreconditionFailedException;
+use JsonApi\Symfony\Http\Exception\PreconditionRequiredException;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,6 +39,30 @@ final class ConditionalRequestEvaluatorTest extends TestCase
         self::assertSame(Response::HTTP_NOT_MODIFIED, $response->getStatusCode());
     }
 
+    public function testIfNoneMatchWildcardTriggersNotModified(): void
+    {
+        $evaluator = $this->createEvaluator();
+        $request = Request::create('/articles', 'GET');
+        $request->headers->set('If-None-Match', '*');
+        $response = new Response();
+
+        $evaluator->evaluate($request, $response, 'hash', null);
+
+        self::assertSame(Response::HTTP_NOT_MODIFIED, $response->getStatusCode());
+    }
+
+    public function testIfNoneMatchMatchesAmongMultipleValidators(): void
+    {
+        $evaluator = $this->createEvaluator();
+        $request = Request::create('/articles', 'GET');
+        $request->headers->set('If-None-Match', '"other", W/"hash"');
+        $response = new Response();
+
+        $evaluator->evaluate($request, $response, 'hash', null);
+
+        self::assertSame(Response::HTTP_NOT_MODIFIED, $response->getStatusCode());
+    }
+
     public function testIfMatchRejectsWeakValidators(): void
     {
         $evaluator = $this->createEvaluator();
@@ -47,6 +72,17 @@ final class ConditionalRequestEvaluatorTest extends TestCase
         $this->expectException(PreconditionFailedException::class);
 
         $evaluator->evaluate($request, new Response(), 'hash', null, false);
+    }
+
+    public function testIfMatchAcceptsUnquotedValidator(): void
+    {
+        $evaluator = $this->createEvaluator();
+        $request = Request::create('/articles', 'PATCH');
+        $request->headers->set('If-Match', 'hash');
+
+        $evaluator->evaluate($request, new Response(), 'hash', null, false);
+
+        $this->addToAssertionCount(1);
     }
 
     public function testIfMatchAcceptsStrongValidators(): void
@@ -60,11 +96,23 @@ final class ConditionalRequestEvaluatorTest extends TestCase
         $this->addToAssertionCount(1);
     }
 
-    private function createEvaluator(): ConditionalRequestEvaluator
+    public function testIfMatchHeaderRequiredWhenConfigured(): void
+    {
+        $evaluator = $this->createEvaluator([
+            'conditional' => ['require_if_match_on_write' => true],
+        ]);
+        $request = Request::create('/articles', 'PATCH');
+
+        $this->expectException(PreconditionRequiredException::class);
+
+        $evaluator->evaluate($request, new Response(), 'hash', null, false);
+    }
+
+    private function createEvaluator(array $config = []): ConditionalRequestEvaluator
     {
         $builder = new ErrorBuilder(true);
         $mapper = new ErrorMapper($builder);
 
-        return new ConditionalRequestEvaluator($mapper);
+        return new ConditionalRequestEvaluator($mapper, $config);
     }
 }
